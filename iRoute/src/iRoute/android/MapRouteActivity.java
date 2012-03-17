150693df416878383aabedb931463676d33ac88c
lasinducharith@gmail.com <lasinducharith@gmail.com@428a6442-1dab-4bcd-57c4-db1d0813e8d0>
2012-03-17 11:54:23 +0000

    Most parts working, database has to be updated

--- iRoute/src/iRoute/android/MapRouteActivity.java
+++ iRoute/src/iRoute/android/MapRouteActivity.java
@@ -1,29 +1,42 @@
 package iRoute.android;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.Currency;
 import java.util.List;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import test3.android.project.R;
+import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Point;
+import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.maps.GeoPoint;
+import com.google.android.maps.ItemizedOverlay;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
 import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
 
 public class MapRouteActivity extends MapActivity {
 
@@ -32,44 +45,167 @@ public class MapRouteActivity extends MapActivity {
 	private Road mRoad;
 	String[] temp;
 	String[] locations;
+	String text;
+	String[] temp2;
+	String[][] cordinates;
+	Double lat, lng;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.mapsview);
 		temp = getIntent().getExtras().getStringArray("locations");
-		locations = new String[temp.length+1];
+		locations = new String[temp.length + 1];
 		locations[0] = getIntent().getExtras().getString("from");
-		
-		for(int i=1;i<temp.length+1;i++){
-			locations[i] = temp[i-1];
-		}	
-	
+
+		for (int i = 1; i < temp.length + 1; i++) {
+			locations[i] = temp[i - 1];
+		}
+		cordinates = new String[locations.length][2];
+		try {
+			for (int j = 0; j < locations.length; j++) {
+				getCordinates(j);
+			}
+			// Toast.makeText(getApplicationContext(), cordinates[0][1],
+			// Toast.LENGTH_LONG).show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		mapView = (MapView) findViewById(R.id.themap);
 		mapView.setBuiltInZoomControls(true);
 
 		new Thread() {
 			@Override
 			public void run() {
-				double fromLat = 6.797325, fromLon = 79.888533, toLat = 6.850823, toLon = 79.865992;
-				String url = RoadProvider
-						.getUrl(fromLat, fromLon, toLat, toLon);
-				InputStream is = getConnection(url);
-				mRoad = RoadProvider.getRoute(is);
-				mHandler.sendEmptyMessage(0);
-				//Toast.makeText(getApplicationContext(), locations[1],Toast.LENGTH_LONG).show();
+				for (int i = 0; i < cordinates.length - 1; i++) {
+					// double fromLat = 6.797325, fromLon = 79.888533, toLat =
+					// 6.850823, toLon = 79.865992;
+					double fromLat = Double.parseDouble(cordinates[i][0]);
+					double fromLon = Double.parseDouble(cordinates[i][1]);
+					double toLat = Double.parseDouble(cordinates[i + 1][0]);
+					double toLon = Double.parseDouble(cordinates[i + 1][1]);
+
+					if (i != cordinates.length - 2) {
+						GeoPoint city = new GeoPoint((int) (fromLat * 1E6),
+								(int) (fromLon * 1E6));
+						Drawable marker = getResources().getDrawable(
+								R.drawable.mapmarker2);
+						int markerWidth = marker.getIntrinsicWidth();
+						int markerHeight = marker.getIntrinsicHeight();
+						marker.setBounds(0, markerHeight, markerWidth, 0);
+
+						MyOverlay myItemizedOverlay = new MyOverlay(marker);
+						mapView.getOverlays().add(myItemizedOverlay);
+						myItemizedOverlay.addItem(city, "City", "City2");
+
+						marker = getResources().getDrawable(
+								R.drawable.mapmarker2);
+						marker.setBounds(0, markerHeight, markerWidth, 0);
+
+					} else {
+						GeoPoint city = new GeoPoint((int) (fromLat * 1E6),
+								(int) (fromLon * 1E6));
+						GeoPoint place = new GeoPoint((int) (toLat * 1E6),
+								(int) (toLon * 1E6));
+
+						Drawable marker = getResources().getDrawable(
+								R.drawable.mapmarker2);
+						int markerWidth = marker.getIntrinsicWidth();
+						int markerHeight = marker.getIntrinsicHeight();
+						marker.setBounds(0, markerHeight, markerWidth, 0);
+
+						MyOverlay myItemizedOverlay = new MyOverlay(marker);
+						mapView.getOverlays().add(myItemizedOverlay);
+						myItemizedOverlay.addItem(city, "City", "City2");
+						myItemizedOverlay = new MyOverlay(marker);
+						mapView.getOverlays().add(myItemizedOverlay);
+						myItemizedOverlay.addItem(place, "City", "City2");
+
+						marker = getResources().getDrawable(
+								R.drawable.mapmarker2);
+						marker.setBounds(0, markerHeight, markerWidth, 0);
+
+					}
+					// GeoPoint place = new GeoPoint((int) (toLat * 1E6),
+					// (int) (toLon * 1E6));
+
+					String url = RoadProvider.getUrl(fromLat, fromLon, toLat,
+							toLon);
+					InputStream is = getConnection(url);
+					mRoad = RoadProvider.getRoute(is);
+					mHandler.sendEmptyMessage(0);
+
+					// Toast.makeText(getApplicationContext(),
+					// locations[1],Toast.LENGTH_LONG).show();
+				}
+
 			}
 		}.start();
+
+		MapController control = mapView.getController();
+		LocationManager manager = (LocationManager) this
+				.getSystemService(Context.LOCATION_SERVICE);
+		LocationListener listner = new LocationListener() {
+
+			@Override
+			public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onProviderEnabled(String arg0) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onProviderDisabled(String arg0) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onLocationChanged(Location arg0) {
+				//lat = arg0.getLatitude();
+				//lng = arg0.getLongitude();
+				lat = 6.788035;
+				lng = 79.885111;
+
+			}
+		};
+		//manager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0,listner);
+		//Location recentLoc = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+		//lat = recentLoc.getLatitude();
+		//lng = recentLoc.getLongitude();
+		lat = 6.711376;
+		lng = 79.907609;
+		GeoPoint curerentLocation = new GeoPoint((int) (lat * 1E6),
+				(int) (lng * 1E6));
+		Drawable markernew = getResources().getDrawable(R.drawable.current);
+		int markernewWidth = markernew.getIntrinsicWidth();
+		int markernewHeight = markernew.getIntrinsicHeight();
+		markernew.setBounds(0, markernewHeight, markernewWidth, 0);
+		MyOverlay mynewOverlay = new MyOverlay(markernew);
+		mapView.getOverlays().add(mynewOverlay);
+		mynewOverlay.addItem(curerentLocation, "Currrent Location",
+				"Current Position");
 	}
 
 	Handler mHandler = new Handler() {
 		public void handleMessage(android.os.Message msg) {
 			TextView textView = (TextView) findViewById(R.id.description);
-			textView.setText(mRoad.mName + " " + mRoad.mDescription);
+			// textView.setText(mRoad.mName + " " + mRoad.mDescription);
+			textView.setText("Bus Route from " + locations[0] + " to "
+					+ locations[locations.length - 1]);
 			MapOverlay mapOverlay = new MapOverlay(mRoad, mapView);
 			List<Overlay> listOfOverlays = mapView.getOverlays();
-			listOfOverlays.clear();
+			// listOfOverlays.clear();
 			listOfOverlays.add(mapOverlay);
+			MapController mc = mapView.getController();
+			mc.setZoom(13);
 			mapView.invalidate();
 		};
 	};
@@ -91,57 +227,92 @@ public class MapRouteActivity extends MapActivity {
 	protected boolean isRouteDisplayed() {
 		return false;
 	}
-}
 
-class MapOverlay extends com.google.android.maps.Overlay {
-	Road mRoad;
-	ArrayList<GeoPoint> mPoints;
+	public void getCordinates(int j) throws IOException {
+		int p = 0;
 
-	public MapOverlay(Road road, MapView mv) {
-		mRoad = road;
-		if (road.mRoute.length > 0) {
-			mPoints = new ArrayList<GeoPoint>();
-			for (int i = 0; i < road.mRoute.length; i++) {
-				mPoints.add(new GeoPoint((int) (road.mRoute[i][1] * 1000000),
-						(int) (road.mRoute[i][0] * 1000000)));
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpPost httppost = new HttpPost(
+				"http://iroute2.byethost32.com/getCordinates.php?locationName="
+						+ URLEncoder.encode(locations[j]));
+
+		try {
+			HttpResponse response = httpclient.execute(httppost);
+			if (response != null) {
+				InputStream is = response.getEntity().getContent();
+
+				BufferedReader reader = new BufferedReader(
+						new InputStreamReader(is));
+				StringBuilder sb = new StringBuilder();
+
+				String line = null;
+				try {
+					while ((line = reader.readLine()) != null) {
+						sb.append(line + "\n");
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				} finally {
+					try {
+						is.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+				text = sb.toString();
+				temp2 = text.split(",");
+				for (String st : temp2) {
+					if (st.length() > 3) {
+
+						cordinates[j][p] = st;
+						p++;
+					}
+
+				}
+
 			}
-			int moveToLat = (mPoints.get(0).getLatitudeE6() + (mPoints.get(
-					mPoints.size() - 1).getLatitudeE6() - mPoints.get(0)
-					.getLatitudeE6()) / 2);
-			int moveToLong = (mPoints.get(0).getLongitudeE6() + (mPoints.get(
-					mPoints.size() - 1).getLongitudeE6() - mPoints.get(0)
-					.getLongitudeE6()) / 2);
-			GeoPoint moveTo = new GeoPoint(moveToLat, moveToLong);
 
-			MapController mapController = mv.getController();
-			mapController.animateTo(moveTo);
-			mapController.setZoom(7);
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
 		}
+
 	}
 
-	@Override
-	public boolean draw(Canvas canvas, MapView mv, boolean shadow, long when) {
-		super.draw(canvas, mv, shadow);
-		drawPath(mv, canvas);
-		return true;
-	}
+	class MyOverlay extends ItemizedOverlay<OverlayItem> {
+		Context context;
 
-	public void drawPath(MapView mv, Canvas canvas) {
-		int x1 = -1, y1 = -1, x2 = -1, y2 = -1;
-		Paint paint = new Paint();
-		paint.setColor(Color.GREEN);
-		paint.setStyle(Paint.Style.STROKE);
-		paint.setStrokeWidth(3);
-		for (int i = 0; i < mPoints.size(); i++) {
-			Point point = new Point();
-			mv.getProjection().toPixels(mPoints.get(i), point);
-			x2 = point.x;
-			y2 = point.y;
-			if (i > 0) {
-				canvas.drawLine(x1, y1, x2, y2, paint);
-			}
-			x1 = x2;
-			y1 = y2;
+		private ArrayList<OverlayItem> overlayItemList = new ArrayList<OverlayItem>();
+
+		public MyOverlay(Drawable marker) {
+			super(boundCenterBottom(marker));
+			populate();
 		}
+
+		public void addItem(GeoPoint p, String title, String snippet) {
+			OverlayItem newItem = new OverlayItem(p, title, snippet);
+			overlayItemList.add(newItem);
+			populate();
+		}
+
+		@Override
+		protected OverlayItem createItem(int i) {
+
+			return overlayItemList.get(i);
+		}
+
+		@Override
+		public int size() {
+
+			return overlayItemList.size();
+		}
+
+		@Override
+		public void draw(Canvas canvas, MapView mapView, boolean shadow) {
+			super.draw(canvas, mapView, shadow);
+
+		}
+
 	}
 }
--- iRoute/src/iRoute/android/MyTable.java
+++ iRoute/src/iRoute/android/MyTable.java
@@ -3,9 +3,9 @@ package iRoute.android;
 import java.util.ArrayList;
 
 import test3.android.project.R;
-import android.R.layout;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.TypedValue;
 import android.view.View;
@@ -65,55 +65,77 @@ public class MyTable extends Activity {
 
 	void fillCountryTable() {
 
-		TableRow row;
-		TextView t1, t2, t3;
+		TableRow row1,row2,row3,row4;
+		TextView t1, t2, t3,t4;
 		// Converting to dip unit
 		int dip = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
 				(float) 1, getResources().getDisplayMetrics());
 
 		for (int current = 0; current < buses.length; current++) {
-			row = new TableRow(this);
+			row1 = new TableRow(this);
+			row2 = new TableRow(this);
+			row3 = new TableRow(this);
+			row4 = new TableRow(this);
 
 			t1 = new TextView(this);
 			// t1.setTextColor(getResources().getColor(R.yellow));
 			t2 = new TextView(this);
 			t3 = new TextView(this);
+			t4 = new TextView(this);
 			// t2.setTextColor(getResources().getColor(R.color.dark_red));
-
+			int now = current+1;
 			t1.setText(buses[current]);
-			t2.setText(from[current]);
-			t3.setText(to[current]);
+			t2.setText("From : "+ from[current]);
+			t3.setText("To : " + to[current]);
+			t4.setText("  ");
+			//t4.setLines(1);
 
 			t1.setTypeface(null, 1);
 			t2.setTypeface(null, 1);
 			t3.setTypeface(null, 1);
+			t4.setTypeface(null, 1);
 
-			t1.setTextSize(12);
-			t2.setTextSize(12);
-			t3.setTextSize(12);
+			t1.setTextSize(16);
+			t2.setTextSize(14);
+			t3.setTextSize(14);
+			t4.setTextSize(12);
+			
+			t2.setTextColor(Color.GRAY);
+			t3.setTextColor(Color.GRAY);
+			
+			t1.setPadding(10 * dip, 0, 0, 0);
+			t2.setPadding(40 * dip, 0, 0, 0);
+			t3.setPadding(40 * dip, 0, 0, 0);
 
 			//t1.setWidth(70 * dip);
 			
-			t1.setWidth(40*dip);
+			//t1.setWidth(40*dip);
 			//t1.setLayoutParams(new TableLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 1f));
 			 //t1.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT));
 		
 			//t2.setWidth(50 * dip);
 			//t2.setHeight(50);
-			t2.setWidth(30*dip);
+			//t2.setWidth(30*dip);
 			//t2.setLayoutParams(new TableLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 1f));
 			//t2.setHorizontallyScrolling(true);
 			//t3.setWidth(50 * dip);
-			t3.setWidth(50 * dip);
+			//t3.setWidth(50 * dip);
 			
 			//t1.setLayoutParams(new TableLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 1f));
 			
 			//t1.setPadding(20 * dip, 0, 0, 0);
-			row.addView(t1);
-			row.addView(t2);
-			row.addView(t3);
-
-			bus_table.addView(row, new TableLayout.LayoutParams(
+			row1.addView(t1);
+			row2.addView(t2);
+			row3.addView(t3);
+			row4.addView(t4);
+		
+			bus_table.addView(row1, new TableLayout.LayoutParams(
+					LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+			bus_table.addView(row2, new TableLayout.LayoutParams(
+					LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+			bus_table.addView(row3, new TableLayout.LayoutParams(
+					LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+			bus_table.addView(row4, new TableLayout.LayoutParams(
 					LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
 
 		}
